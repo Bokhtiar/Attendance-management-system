75b2f0ddd38398aef1b598e8ad2238bf1b7394bc
Bokhtiar <bokhtiartoshar1@gmail.com>
2023-01-24 18:27:51 +0600

    Role network created

---
+++ app/Traits/Network/RoleNetwork.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Traits\Network;
+
+use App\Models\Role;
+
+trait RoleNetwork
+{
+    /* display a resouce list */
+    public function RoleList()
+    {
+        return Role::all();
+    }
+
+    /* store a newly resource*/
+    public function ResourceStoreRole($request, $role = null)
+    {
+        return array(
+            'name' => $request->password,
+        );
+    }
+
+    /* store resource */
+    public function UserStore($request)
+    {
+        return Role::create($this->ResourceStoreRole($request));
+    }
+
+    /* single resource show */
+    public function RoleFindById($role_id)
+    {
+        return Role::find($role_id);
+    }
+
+    /* resource update */
+    public function RoleUpdate($request, $id)
+    {
+        $role = Role::find($id);
+        return $role->update($this->ResourceStoreRole($request, $role));
+    }
+}
