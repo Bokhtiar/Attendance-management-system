61472537246eb3ea67621b1a534773ff09ebc71b
Bokhtiar <bokhtiartoshar1@gmail.com>
2023-01-25 11:50:55 +0600

    Department database created

--- resources/views/layouts/partial/sidebar.blade.php
+++ resources/views/layouts/partial/sidebar.blade.php
@@ -10,27 +10,27 @@
         </li><!-- End Dashboard Nav -->
 
         @isset(auth()->user()->role->permission['permission']['user']['list'])
-        <li class="nav-item">
-            <a class="nav-link collapsed" data-bs-target="#Bed-manager-nav" data-bs-toggle="collapse" href="#">
-                <i class="bi bi-menu-button-wide"></i><span>Employee</span><i class="bi bi-chevron-down ms-auto"></i>
-            </a>
-            <ul id="Bed-manager-nav" class="nav-content collapse " data-bs-parent="#sidebar-nav">
-               @isset(auth()->user()->role->permission['permission']['user']['list'])
-                <li>
-                    <a href="@route('user.index')">
-                        <i class="bi bi-circle"></i><span>Employee</span>
-                    </a>
-                </li>
-                @endisset
-                @isset(auth()->user()->role->permission['permission']['user']['add'])
-                <li>
-                    <a href="@route('user.create')">
-                        <i class="bi bi-circle"></i><span>Employee Create</span>
-                    </a>
-                </li>
-                @endisset
-            </ul>
-        </li><!-- End employee Nav -->
+            <li class="nav-item">
+                <a class="nav-link collapsed" data-bs-target="#Bed-manager-nav" data-bs-toggle="collapse" href="#">
+                    <i class="bi bi-menu-button-wide"></i><span>Employee</span><i class="bi bi-chevron-down ms-auto"></i>
+                </a>
+                <ul id="Bed-manager-nav" class="nav-content collapse " data-bs-parent="#sidebar-nav">
+                    @isset(auth()->user()->role->permission['permission']['user']['list'])
+                        <li>
+                            <a href="@route('user.index')">
+                                <i class="bi bi-circle"></i><span>Employee</span>
+                            </a>
+                        </li>
+                    @endisset
+                    @isset(auth()->user()->role->permission['permission']['user']['add'])
+                        <li>
+                            <a href="@route('user.create')">
+                                <i class="bi bi-circle"></i><span>Employee Create</span>
+                            </a>
+                        </li>
+                    @endisset
+                </ul>
+            </li><!-- End employee Nav -->
         @endisset
 
 
@@ -40,19 +40,19 @@
             </a>
             <ul id="setting" class="nav-content collapse " data-bs-parent="#sidebar-nav">
                 @isset(auth()->user()->role->permission['permission']['role']['list'])
-                <li>
-                    <a href="@route('role.index')">
-                        <i class="bi bi-circle"></i><span>Role</span>
-                    </a>
-                </li>
+                    <li>
+                        <a href="@route('role.index')">
+                            <i class="bi bi-circle"></i><span>Role</span>
+                        </a>
+                    </li>
                 @endisset
 
                 @isset(auth()->user()->role->permission['permission']['permission']['list'])
-                <li> 
-                    <a href="@route('permission.index')">
-                        <i class="bi bi-circle"></i><span>Permission</span>
-                    </a>
-                </li>
+                    <li>
+                        <a href="@route('permission.index')">
+                            <i class="bi bi-circle"></i><span>Permission</span>
+                        </a>
+                    </li>
                 @endisset
 
             </ul>
