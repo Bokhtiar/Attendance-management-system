8669b12e573ceb27f4ca856f0bbe0f76af6f751f
Bokhtiar <bokhtiartoshar1@gmail.com>
2023-01-26 12:48:24 +0600

    Leave application database desng

--- app/Models/Leave.php
+++ app/Models/Leave.php
@@ -7,5 +7,20 @@ use Illuminate\Database\Eloquent\Model;
 
 class Leave extends Model
 {
-    use HasFactory;
+    use HasFactory; 
+    protected $table = 'leaves';
+    protected $primaryKey = 'leave_id';
+
+    protected $fillable = [
+        'user_id',
+        'resone',
+        'start_date',
+        'end_date',
+    ];
+
+    /* reletion pepartment */
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
 }
--- database/migrations/2023_01_26_123856_create_leaves_table.php
+++ database/migrations/2023_01_26_123856_create_leaves_table.php
@@ -14,7 +14,12 @@ return new class extends Migration
     public function up()
     {
         Schema::create('leaves', function (Blueprint $table) {
-            $table->id();
+            $table->id('leave_id');
+            $table->integer('user_id');
+            $table->longText('resone')->required();
+            $table->string('start_date');
+            $table->string('end_date');
+            $table->tinyInteger('is_approved')->default(0);
             $table->timestamps();
         });
     }
